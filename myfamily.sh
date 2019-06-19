3d6870fb90f1881878790e94b9858be65cc5fa40
ali <followmysiteplease@gmail.com>
2019-06-19 21:00:20 +0600

    first

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq ".[$HERO_ID -1][\"connections\"][\"relatives\"]" | tr -d "\""
+curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq ".[$HERO_ID][\"connections\"][\"relatives\"]" | tr -d "\""
